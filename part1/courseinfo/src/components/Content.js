17de16e7b4e2ed046dc4ca3ce438ade33a3fd42b
miueon <cruxbell@gmail.com>
2022-09-24 18:24:09 +0800

    course info s7

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Content = ({ parts }) => {
-    console.log(parts)
+    const total = parts.reduce((prv, cv) => prv + cv.exercises, 0)
     return (
         <div>
             {parts.map(content =>
                 <p key={content.id}>{content.name} {content.exercises}</p>
             )}
+            <b>total of {total} exercises</b>
         </div>
     )
 }
